6b27b0f64f1acfb09ca42ce0d3a10a594da5a12a
aman mishra <12defence2005@gmail.com>
2023-12-02 21:31:08 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CourseList from './components/CourseList';
 import CourseDetails from './components/CourseDetails';
 import StudentDashboard from './components/StudentDashboard';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={CourseList} />
-        <Route path="/course/:id" component={CourseDetails} />
-        <Route path="/dashboard" component={StudentDashboard} />
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CourseList/>} />
+        <Route path="/course/:id" element={<CourseDetails/>} />
+        <Route path="/dashboard" element={<StudentDashboard/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
